9ccb8af417ce28f8cc9c30b4b796bf5b9888c00e
Mike Wang <mike.wang7@gmail.com>
2024-02-01 15:14:25 -0800

    more cleanup

--- wordle.py
+++ wordle.py
@@ -1,31 +1,23 @@
 from random import randint
 
 # definition of colour escape sequences
-GREEN_LETTER = "\033[1;42m "
-YELLOW_LETTER = "\033[1;43m "
-GREY_LETTER = "\033[1;49m "
+GREEN_LETTER = "\033[1;42m"
+YELLOW_LETTER = "\033[1;43m"
+GREY_LETTER = "\033[1;49m"
 STOP = "\033[0m"
 
 MAX_GUESSES = 6
 
-def invalid_input(word):
-    if word.isalpha():
-        return False if len(word) == 5 and word.lower() in words else True
-    return True
-
-
 # calculates the "correctness" of word, prints the coloured result to console
 def evaluate(word):
     duplicates = {}
-    score = [None, None, None, None, None]
+    score = [""] * 5
     for i in range(5):
         letter = word[i]
         if letter == answer[i]:
-            score[i] = GREEN_LETTER + letter
+            score[i] = f"{GREEN_LETTER} {letter} "
             duplicates.setdefault(letter, 0)
             duplicates[letter] += 1
-        else:
-            score[i] = ""
 
     for i in range(5):
         letter = word[i]
@@ -34,13 +26,12 @@ def evaluate(word):
         duplicates.setdefault(letter, 0)
         duplicates[letter] += 1
         if letter in answer and duplicates[letter] <= answer.count(letter):
-            score[i] = YELLOW_LETTER + letter
+            score[i] = f"{YELLOW_LETTER} {letter} "
         else:
-            score[i] = GREY_LETTER + letter
-
-    for i in score:
-        print(i + " ", end="")
-    print(STOP)
+            score[i] = f"{GREY_LETTER} {letter} "
+    
+    score.append(STOP)
+    print("".join(score))
 
     if word == answer:
         print("Congrats, you guessed correctly!")
@@ -52,23 +43,22 @@ if __name__ == '__main__':
 
     with open("sgb-words.txt", "r") as f:
         words = f.read().splitlines()
-        answer_idx = randint(0, len(words) - 1)
-        answer = words[answer_idx].upper()
+        answer = words[randint(0, len(words)-1)].upper()
         words = set(words)
 
     guess = ""
     num_guesses = 1
 
     while True:
-        guess = input(f"({num_guesses}/6) Guess: ").upper()
-        if invalid_input(guess):
-            print("Word not in list. Try again")
-            continue
-        else:
+        guess = input(f"({num_guesses}/6) Guess: ")
+        if guess.isalpha() and guess.lower() in words:
             num_guesses += 1
-            if evaluate(guess):
+            if evaluate(guess.upper()):
                 break
             if num_guesses > MAX_GUESSES:
                 print("Out of tries, game over!")
                 print(f"The word was {answer}")
                 break
+        else:
+            print("Word not in list. Try again")
+
